3c4fbf20d96b57bb44196f356cad9e4f1587eca1
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-07 16:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -99,8 +99,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === 'undefined') {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
-
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    var tot = total();
+    console.log(`Your total cost is $${tot}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   }
 }
