4e7923732ea466903064791f11f4e112064610bb
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-07 16:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -100,5 +100,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     
+    console.log(`Your total cost is $71, which will be charged to the card ${cardNumber}.`);
   }
 }
