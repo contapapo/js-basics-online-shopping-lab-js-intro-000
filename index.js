386fd2effc5e28d46bc9068c0e0b88f287bcc740
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-07 15:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i,1);
     }
-    else {
+    else if (!cart[i].hasOwnProperty(item)){
       console.log("That item is not in your cart.");
     }
   }
